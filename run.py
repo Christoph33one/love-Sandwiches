f38b4614a8c641c0e2a09188816d623df29fe92d
Christoph33one <cjbrowning23@hotmail.com>
2022-09-18 13:35:05 +0000

    add user request loop to repeat request for the data until data provided is valid

--- run.py
+++ run.py
@@ -17,17 +17,23 @@ def get_sales_data():
     """
     get sales figures from the user
     """
-    print("please enter sales data from last market")
-    print("data should be six numbers, seperated be commas.")
-    print("example: 10,20,30,40,50,60\n")
+    while True:
+        print("please enter sales data from last market")
+        print("data should be six numbers, seperated be commas.")
+        print("example: 10,20,30,40,50,60\n")
 
     # data_str function for data as a string. 
     # print data_str back to the termial to check its value. 
     # (always print functions to test it works!) 
-    data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:") 
 
-    sales_data = data_str.split(",")  # commis seperate values in the list.
-    validate_data(sales_data)
+        sales_data = data_str.split(",")  # split(",") seperate values in the list.
+
+        if validate_data(sales_data):
+            print("Data is valid")
+            break # stops loop if no Errors. (print(True))
+
+    return sales_data
 
 def validate_data(values):
     """
@@ -35,9 +41,9 @@ def validate_data(values):
     Raises ValueError if string cannot be converted into intergers,
     or if there are not 6 values.
     """
-# try statement lets you test for Errors in a blocl of code. 
+# try statement lets you test for Errors in a blocl of code, 
 # Here an if statement is added.
-    print(values)
+    
     try: 
         [int(value) for value in values]
         if len(values) != 6: # if list length is not equal to 6.
@@ -46,6 +52,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again./n")
+        return False
+
+    return True
 
 # calls first function to get sales data from the terminal.
-get_sales_data()
+data = get_sales_data()
